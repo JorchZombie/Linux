79bc2c90fa1443189fadc98322a0d97c42c391ca
Jorge González <98619897+JorchZombie@users.noreply.github.com>
2025-07-23 20:35:50 -0600

    Delete nvim directory

--- nvim/lua/custom/configs/jdtls.lua
+++
@@ -1,4 +0,0 @@
-return{
-  cmd = {"jdtls"},
-  root_dir = require("lspconfig").util.root_pattern(".git", "mvnw", "gradlew", "build.gradle", "pom.xml")
-}
--- nvim/lua/custom/configs/pyright.lua
+++
@@ -1,9 +0,0 @@
-return {
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "off"
-      }, --termina analysis
-    },-- termina python
-  }, --termina settings
-}--termina return
